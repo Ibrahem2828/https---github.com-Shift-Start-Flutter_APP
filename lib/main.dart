8e21334d0f03c0c9ff591e91c48bbe2dc146c5f5
ibrahim <113996610+Ibrahem2828@users.noreply.github.com>
2024-11-24 15:51:12 +0300

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -50,6 +50,7 @@ class _MyAppState extends State<MyApp> {
             theme: ThemeData(),
 
             debugShowCheckedModeBanner: false,
+            
             // home:SettingsScreen()
           );
         }));
